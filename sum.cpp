b3565cdb622bffa0c7bad23888ffe0526ba4caf7
hafsakk06-png <hafsakk06@gmail.com>
2025-08-31 04:30:59 -0700

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 27, b = 36, sum;
+    //calculation
+    sum = a + b;
+
+    //output results
+    cout <<"sum = " << sum << endl;
+    return 0;
+}
