71dd77ecef2fd543cd64aeef7c83621a23bd3c6e
hafsakk06-png <hafsakk06@gmail.com>
2025-09-05 04:44:49 -0700

    Add files via upload

---
+++ low-uppercase.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cout <<"enter ch:";
+    cin >> ch;
+
+    if(ch >= 'a' && ch <= 'z') {
+        cout <<"lowercase\n";
+
+    } else {
+        cout <<"uppercase\n";
+    }
+
+    return 0;
+}
